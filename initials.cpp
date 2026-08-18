85864e08064cd6e8dd7be3b5fdf19616e398d685
Dell <robert.maksymow@tutanota.com>
2026-08-18 15:26:08 +0100

    Print initials

---
+++ initials.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "RRRR   M   M\n";
+  std::cout << "R   R  MM MM\n";
+  std::cout << "R   R  MM MM\n";
+  std::cout << "RRRR   M M M\n";
+  std::cout << "R R    M   M\n";
+  std::cout << "R  R   M   M\n";
+  std::cout << "R   R  M   M\n";
+
+  return 0;
+
+}
