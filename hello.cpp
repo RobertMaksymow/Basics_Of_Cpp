519094e165e97a01f1df929b24672c98f5624ba4
Dell <robert.maksymow@tutanota.com>
2026-08-18 14:50:56 +0100

    Added comments

--- hello.cpp
+++ hello.cpp
@@ -3,4 +3,20 @@
 int main() {
   std::cout << "Hello World!\n";
   std::cout << "Goodbye\n";
+
+  return 0;
 }
+
+// To run this program compile it first and then execute the generated binary. You can compile the program using a C++ compiler like g++. For example, you can use the following command in the terminal:
+// g++ hello.cpp -o hello
+// This command compiles the hello.cpp file and generates an executable named hello. You can then run the program by executing the following command:
+// ./hello
+
+// The #include directive is used to include the contents of a file or library in the program. In this case, we are including the iostream library, which provides input and output functionality.
+
+// The main() function is the entry point of the program. It is where the execution of the program begins. The return type of the main() function is int, which means it returns an integer value to the operating system when the program finishes executing.
+
+// The std::cout object is used to output data to the standard output device (the screen). The << operator is used to send the data to the output stream. The \n character is used to insert a new line after the output.
+
+// The return statement is used to end a function. If the program reaches this statement, returning a value of 0 is an indication to the operating system that the code executed successfully. This line of code is optional.
+
