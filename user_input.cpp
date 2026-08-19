412802fd948742dfcc4ee51e16975d0051c5a5a1
Dell <robert.maksymow@tutanota.com>
2026-08-19 13:57:33 +0100

    Variables and user input/output

---
+++ user_input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  
+  int tip = 0;
+  
+  std::cout << "Enter tip amount: ";
+  std::cin >> tip; //character input by user
+  std::cout << "You paid " << tip << " dollars.\n";
+  return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+  int score = 0;
+  std::cout << "Player score: " << score << "\n";
+
+    int age = 28;
+    std::cout << "Hello, I am ";
+    std::cout << age;
+    std::cout << " years old\n";
+
+    // Variable chaining
+    age = 45;
+    std::cout << "Hello, I am " << age << " years old\n";
+
+  return 0; 
+}
